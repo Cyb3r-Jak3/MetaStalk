87fb742a6a17e8c67eed75f330effdbf527f2cbe
Jacob <jake@jwhite.network>
2019-11-11 13:33:30 -0500

    - Moved Example Photos to a test folder. (Tests in progress) - Created the GPS Checking as a function

--- PyStalk.py
+++ PyStalk.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # pylint: disable=invalid-name, missing-module-docstring, missing-function-docstring,
 import argparse
 import os
@@ -8,48 +9,58 @@ import plotly.express as px
 
 log = logging.getLogger(__name__)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('files', nargs='*', default=None,
-                    help='Directory of photos to check')
-args = parser.parse_args()
-
-for path in args.files:
-    isdir = os.path.isdir(path)
-
 
 def dms2dd(degrees, minutes, seconds):
     dd = float(degrees) + float(minutes)/60 + float(seconds)/(60*60)
     return dd
 
 
-photos = []
-if isdir:
-    for file in os.listdir(args.files[0]):
-        photos.append(os.path.join(args.files[0], file))
-else:
-    for item in args.files:
-        photos.append(args.files[0])
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('files', nargs='*', default=None,
+                        help='Path of photos to check')
+    parser.add_argument('test', default=False, action="store_true",
+                        help='True or false. Set to true to hide the figure.')
+    args = parser.parse_args()
 
-lats = []
-longs = []
-gps_photos = []
+    for path in args.files:
+        isdir = os.path.isdir(path)
 
-for each in photos:
-    with open(each, 'rb') as image_file:
-        my_image = Image(image_file)
-        if my_image.has_exif:
-            gps_photos.append(each)
-            lats.append(dms2dd(*my_image.gps_latitude))
-            longs.append(dms2dd(*my_image.gps_longitude))
-            log.debug("{} has exif gps data".format(each))
+    photos = []
+    if isdir:
+        for file in os.listdir(args.files[0]):
+            photos.append(os.path.join(args.files[0], file))
+    else:
+        for item in len(range(args.files)):
+            photos.append(args.files[item])
+
+    def GPS_Check():
+        lats = []
+        longs = []
+        gps_photos = []
+
+        for each in photos:
+            with open(each, 'rb') as image_file:
+                my_image = Image(image_file)
+                if my_image.has_exif:
+                    gps_photos.append(each)
+                    lats.append(dms2dd(*my_image.gps_latitude))
+                    longs.append(dms2dd(*my_image.gps_longitude))
+                    log.debug("{} has exif gps data".format(each))
+                else:
+                    log.info("{} has no exif data ".format(each))
+        points = []
+        for x in range(len(lats)):
+            points.append((lats[x], longs[x]))
+
+        fig = px.scatter_mapbox(lon=longs, lat=lats, hover_name=gps_photos)
+        fig.update_layout(mapbox_style="open-street-map",
+                          title="Geo Locations",)
+        if args.test:
+            pass
         else:
-            log.info("{} has no exif data ".format(each))
+            fig.show()
+    GPS_Check
 
-points = []
-for x in range(len(lats)):
-    points.append((lats[x], longs[x]))
-
-
-fig = px.scatter_mapbox(lon=longs, lat=lats, hover_name=gps_photos)
-fig.update_layout(mapbox_style="open-street-map", title="Geo Locations",)
-fig.show()
+if __name__ == "__main__":
+    main()
