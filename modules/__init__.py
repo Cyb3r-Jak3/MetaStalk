c311f00f542a43c32477402efb3eb7852f272935
Jacob <jake@jwhite.network>
2019-12-15 18:14:09 -0500

    Forgot to commit this file

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,13 +1,11 @@
 # pylint: disable=missing-module-docstring
 from .GPSCheck import GPS_Check
-from .Models import Model_Chart
 from .PhotoTable import Stats
 from .DateTime import DateTime
 from .Flash import Flash_Chart
 from .Focal import Focal_Chart
-from .Software import Software_Chart
 from .PieChart import Pi_Chart
 
-__all__ = ["GPS_Check", "Model_Chart", "Stats",
-           "DateTime", "Flash_Chart", "Focal_Chart", 'Software_Chart',
+__all__ = ["GPS_Check", "Stats", "DateTime",
+           "Flash_Chart", "Focal_Chart",
            "Pi_Chart"]
