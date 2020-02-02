f601339d3bdb2f5b28ea1ed0eb6c544ab90789b2
Jacob <jake@jwhite.network>
2020-02-02 10:59:13 -0500

    Rewrote to use hachoir

--- utils/web.py
+++ utils/web.py
@@ -6,21 +6,21 @@ import dash_html_components as html
 import dash_core_components as dcc
 
 
-def graph(plots, log, start, test=False):
+def graph(plots, log, t_start, test=False):
     """Creates the graphs"""
     external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
     app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-    app.title = "PyStalk | Dash"
+    app.title = "PyStalk"
     graphs = []
     for name, chart in plots.items():
         graphs.append(dcc.Graph(id="graph-{}".format(name), figure=chart))
-    stop = timeit.default_timer()
+    t_stop = timeit.default_timer()
     app.layout = html.Div([
         html.H1("PyStalk", style={"textAlign": "center"}),
         html.H6(html.A('Cyber Jake', href="https://twitter.com/Cyb3r_Jak3"),
                 style={"textAlign": "center"}),
         html.Div(children=graphs),
-        html.P("Time Taken = {0:.2f} seconds".format(stop - start),
+        html.P("Time Taken = {0:.2f} seconds".format(t_stop - t_start),
                style={"textAlign": "center"})
         ])
     if not test:
@@ -31,4 +31,4 @@ def graph(plots, log, start, test=False):
             log.info("Interput received. Exiting.")
     else:
         log.info("Test flag was set. No webpage will be shown.")
-        log.info("Time Taken = {0:.2f} seconds".format(stop - start))
+        log.info("Time Taken = {0:.2f} seconds".format(t_stop - t_start))
